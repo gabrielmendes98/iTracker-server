faf16a28b906911fa86135b1aa0603dec39b5112
Gabriel <gabrielmssantiago@gmail.com>
2020-05-08 20:28:36 -0300

    restore issue

--- resolvers/issue.js
+++ resolvers/issue.js
@@ -85,6 +85,19 @@ async function remove(_, { id }) {
   return false;
 }
 
+async function restore(_, { id }) {
+  const db = getDb();
+  const issue = await db.collection('deleted_issues').findOne({ id });
+  if (!issue) return false;
+  issue.deleted = new Date();
+  let result = await db.collection('issues').insertOne(issue);
+  if (result.insertedId) {
+    result = await db.collection('deleted_issues').removeOne({ id });
+    return result.deletedCount === 1;
+  }
+  return false;
+}
+
 async function counts(_, { status, effortMin, effortMax }) {
   const db = getDb();
   const filter = {};
@@ -120,4 +133,4 @@ async function counts(_, { status, effortMin, effortMax }) {
   return Object.values(stats);
 }
 
-module.exports = { list, add, get, update, remove, delete: remove, counts };
+module.exports = { list, add, get, update, remove, delete: remove, counts, restore };
